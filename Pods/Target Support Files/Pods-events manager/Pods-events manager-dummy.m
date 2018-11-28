d2e1114aced00a07eb142a6c566d5bc516eada90
nicolaserny <nicolas.erny@xcomponent.com>
2018-11-28 14:09:48 +0100

    ISSUE_8: init xcode projects - create a basic single page project with xcode - configure cocoapod

---
+++ Pods/Target Support Files/Pods-events manager/Pods-events manager-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_events_manager : NSObject
+@end
+@implementation PodsDummy_Pods_events_manager
+@end
---
+++ Pods/Target Support Files/Pods-events managerTests/Pods-events managerTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_events_managerTests : NSObject
+@end
+@implementation PodsDummy_Pods_events_managerTests
+@end

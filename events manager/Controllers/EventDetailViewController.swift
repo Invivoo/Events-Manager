6d1d4dc602cbe86e131c85218522cea7e9687ad5
nicolaserny <nicolas.erny@xcomponent.com>
2018-12-07 17:32:49 +0100

    Add master/details design - Configure a SplitViewController - Create an empty EventDetailViewController

---
+++ events manager/Controllers/EventDetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  EventDetailViewController.swift
+//  events manager
+//
+//  Created by Nicolas Erny on 07/12/2018.
+//  Copyright © 2018 Invivoo. All rights reserved.
+//
+
+import UIKit
+
+class EventDetailViewController: UIViewController {
+
+    var detailItem: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}

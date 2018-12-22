685fb3ede6681b06b95a2b978968d7829509f2bb
nicolaserny <nicolas.erny@xcomponent.com>
2018-12-22 19:47:06 +0100

    Add basic navigation tests

--- events managerUITests/events_managerUITests.swift
+++ events managerUITests/events_managerUITests.swift
@@ -18,7 +18,6 @@ class events_managerUITests: XCTestCase {
 
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
         XCUIApplication().launch()
-
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
@@ -26,9 +25,12 @@ class events_managerUITests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testBasicNavigation() {
+        let app = XCUIApplication()
+        app.tables/*@START_MENU_TOKEN@*/.staticTexts["Event1"]/*[[".cells.staticTexts[\"Event1\"]",".staticTexts[\"Event1\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        if UIDevice.current.userInterfaceIdiom == .phone{
+            app.navigationBars["Event"].buttons["Events Manager"].tap()
+        }
     }
 
 }
